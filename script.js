b9ea1d74e86910e84465c984a2c84fdda2d61044
wodawodawoda <mateusz.wodnik@gmail.com>
2018-02-11 16:18:02 +0100

    Fix script validation error

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-var a = 10
-	b = 5
+var a = 10;
+	b = 5;
 	value = (a * a) - (2 * a * b) - (b * b);
 
 console.log(value);
 
 //Classic if else
 if (value < 0) {
-	console.log('wynik ujemny')
+	console.log('wynik ujemny');
 } else if (value > 0) {
-	console.log('wynik dodatni')
+	console.log('wynik dodatni');
 } else {
-	console.log('wynik równy zero')
-};
+	console.log('wynik równy zero');
+}
 
 var checkNumber = value < 0 ? 'wynik ujemny' : value > 0 ? 'wynik dodatni' : 'wynik równy zero';
 console.log(checkNumber);
 
 //Shorthand if else statement
-value < 0 ? console.log('wynik ujemny') : value > 0 ? console.log('wynik dodatni') : console.log('wynik równy zero');
-
-
+var shortHand = value < 0 ? 'wynik ujemny' : value > 0 ? 'wynik dodatni' : 'wynik równy zero';
+console.log(shortHand);
 
 
 
